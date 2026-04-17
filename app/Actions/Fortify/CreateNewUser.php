b3ea9acb50d977f9921275b8ac7c654bb26141e1
Allan Pedernal <allanpedernal@gmail.com>
2026-04-17 20:11:51 +0800

    update create new user to have firstnane and lastname

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -4,9 +4,14 @@ namespace App\Actions\Fortify;
 
 use App\Concerns\PasswordValidationRules;
 use App\Concerns\ProfileValidationRules;
+use App\Enums\UserRole;
+use App\Models\Customer;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Spatie\Permission\Models\Role;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -20,14 +25,33 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input): User
     {
         Validator::make($input, [
-            ...$this->profileRules(),
+            ...$this->registrationRules(),
             'password' => $this->passwordRules(),
         ])->validate();
 
-        return User::create([
-            'name' => $input['name'],
-            'email' => $input['email'],
-            'password' => $input['password'],
-        ]);
+        return DB::transaction(function () use ($input) {
+            $firstName = trim($input['first_name']);
+            $lastName = trim($input['last_name']);
+            $fullName = trim("{$firstName} {$lastName}");
+
+            $customer = Customer::create([
+                'code' => 'CUST-'.strtoupper(Str::random(6)),
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+                'email' => $input['email'],
+            ]);
+
+            $user = User::create([
+                'name' => $fullName,
+                'email' => $input['email'],
+                'password' => $input['password'],
+            ]);
+
+            Role::findOrCreate(UserRole::Customer->value, 'web');
+            $user->assignRole(UserRole::Customer->value);
+            $user->customers()->attach($customer->id);
+
+            return $user;
+        });
     }
 }

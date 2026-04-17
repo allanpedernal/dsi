6c3f5ac7ab6e415602edfdc5ef5bde0d999bd860
Allan Pedernal <allanpedernal@gmail.com>
2026-04-17 20:19:38 +0800

    create policies

---
+++ app/Policies/SalePolicy.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Sale;
+use App\Models\User;
+
+class SalePolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return $user->can('sales.view');
+    }
+
+    public function view(User $user, Sale $sale): bool
+    {
+        return $user->can('sales.view');
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->can('sales.create');
+    }
+
+    public function update(User $user, Sale $sale): bool
+    {
+        return $user->can('sales.update');
+    }
+
+    public function delete(User $user, Sale $sale): bool
+    {
+        return $user->can('sales.delete');
+    }
+
+    public function refund(User $user, Sale $sale): bool
+    {
+        return $user->can('sales.refund');
+    }
+}
